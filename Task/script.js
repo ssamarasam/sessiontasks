24fd72ef6e8836c79b1290f695c97397a9dc13bc
ssamarasam <samcsegct@gmail.com>
2023-04-01 23:52:23 +0530

    new session tasks

---
+++ Task/script.js
@@ -0,0 +1,14 @@
+// alert('Hey everyone!');
+
+
+// let arr = [5, 4, 3, 2, 1];
+
+// let sum = arr.reduce((acc, item) => acc + item);
+// console.log(sum);
+
+
+console.log(12 / 2 + ' partitions');
+console.log(12 / 3 + ' partitions');
+console.log(12 / 4 + ' partitions');
+console.log(12 / 6 + ' partitions');
+console.log(12 / 12 + ' partitions');
